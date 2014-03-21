86a835dc6a93194d2c92c3744dfd2694b2f0156a
Jessica Brazelton <jbrazelt@thoughtworks.com>
2014-03-22 02:21:58 +1100

    Jessica - Added bootstrap.

---
+++ src/main/java/com/yeasoundsgood/app/HomeController.java
@@ -0,0 +1,21 @@
+package com.yeasoundsgood.app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+	@RequestMapping(method = RequestMethod.GET)
+	public String printWelcome(ModelMap model) {
+		model.addAttribute("message", "Hello world!");
+		return "hello";
+	}
+
+    @RequestMapping(method = RequestMethod.GET, value = "theme")
+    public String showTheme() {
+        return "bootstrap_theme";
+    }
+}
